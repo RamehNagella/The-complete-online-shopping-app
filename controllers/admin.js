91f17dc2228639a233f72c1c9e05782bc6b95ce9
RameshNagella <rameshnagella272@gmail.com>
2023-06-29 16:18:50 +0530

    Initial commit

---
+++ controllers/admin.js
@@ -0,0 +1,78 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admim/add-product",
+    editing: false,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+
+  const product = new Product(null, title, imageUrl, price, description);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+
+  if (!editMode) {
+    res.redirect("/");
+  }
+  const prodId = req.params.productId;
+
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    console.log(product);
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admim/edit-product",
+      editing: editMode,
+      product: product,
+    });
+  });
+};
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const upatedImageUrl = req.body.imageUrl;
+  const updtedPrice = req.body.price;
+  const updatedDesc = req.body.description;
+
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    upatedImageUrl,
+    updtedPrice,
+    updatedDesc
+  );
+  updatedProduct.save();
+  res.redirect("/admin/products");
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    console.log("@@@>>", products);
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log("%%%>>>", prodId);
+  Product.deleteById(prodId);
+
+  res.redirect("/admin/products");
+};
